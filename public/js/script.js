fc800f7c1281a992e8b2b9a8726a0652f5966d9c
sherwino <sherwination@gmail.com>
2018-09-15 13:32:23 -0400

    Added linting settings, and packages along with sample code

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,14 @@
-'use strict';
 
+
+const poop = {
+    test: 'the linter out',
+};
+
+const poopAgain = (object) => {
+    const theLint = `I am going to ${object}`;
+
+    return theLint;
+};
+
+
+poopAgain(poop);
