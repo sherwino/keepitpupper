99be4f8ca45a5439f023ba950f8ce6f300f49dfb
javif89 <javier0eduardo@hotmail.com>
2019-02-06 17:07:10 -0500

    FIRESTORE

--- src/components/Editable.vue
+++ src/components/Editable.vue
@@ -3,16 +3,44 @@
 </template>
 
 <script>
+    let firebase = require('firebase');
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAcjW2rWja6VayhZa90WQVu56NPbS-yq5Q",
+        authDomain: "keepitpupper-dev.firebaseapp.com",
+        databaseURL: "https://keepitpupper-dev.firebaseio.com",
+        projectId: "keepitpupper-dev",
+        storageBucket: "keepitpupper-dev.appspot.com",
+        messagingSenderId: "212309711746"
+    };
+
+    let app = firebase.initializeApp(config);
+    let firestore = app.firestore();
+
     export default {
         name: 'editable',
         props: ['docKey'],
         data: function () {
-            return {};
+            return {
+                content: "Content goes here"
+            };
         },
         computed: {
             content() {
-                return 'Editable content';
             }
+        },
+        async mounted() {
+            let docRef = firestore.doc(this.docKey);
+            let content = await docRef.get();
+            let value = content.data().value;
+            this.content = value;
+
+            //Set up listener
+            docRef.onSnapshot((doc) => {
+                let value = doc.data().value;
+                this.content = value;
+            });
         },
         methods: {}
     };
