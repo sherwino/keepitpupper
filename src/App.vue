4d4de30f011960ea43431841aff3df64f1f0dc1e
sherwino <sherwino@outlook.com>
2019-02-06 00:23:18 -0500

    Initial spotify sample
    Initial spotify sample
    
    Spotify sample

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="This branch has sample code from Firebase on how to setup Spotify Auth"/>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header mdl-color-text--white mdl-color--light-blue-700">
+        <div class="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">
+          <div
+            class="mdl-layout__header-row mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-cell--8-col-desktop"
+          >
+            <h3>See more below</h3>
+            <a
+              href="https://github.com/firebase/functions-samples/tree/Node-8/spotify-auth"
+            >Firebase Auth w/ Spotify</a>
+          </div>
+        </div>
+      </header>
+    </div>
   </div>
 </template>
 
@@ -11,6 +25,7 @@ export default {
     msg: String,
   },
 };
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
