169366aefb129999833c02e79f388a3657f557b3
javif89 <javier0eduardo@hotmail.com>
2019-02-06 15:19:43 -0500

    Started setting up the base editable component

---
+++ src/components/Editable.vue
@@ -0,0 +1,29 @@
+<template>
+    <div contenteditable="true">
+        <slot v-bind:content="content">
+            {{ content }}
+        </slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'editable',
+        props: {
+            docKey: String
+        },
+        data: function () {
+            return {};
+        },
+        computed: {
+            content() {
+                return 'This is the content';
+            }
+        },
+        methods: {}
+    };
+</script>
+
+<style lang="scss">
+
+</style>
