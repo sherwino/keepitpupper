4275c97757d138f959fa1268ee3077154a1c8bf6
Sherwino <sherwino@outlook.com>
2019-09-24 09:14:48 -0400

    Adding artists (#32)
    * Updated images
    
    * Adding artists to slider, and formatting

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,106 @@
 <template>
-	<div>
-		<nav class="navbar navbar-expand-lg navbar-light bg-white navbar-transparent-light navbar-sticky">
-			<div class="container">
-				<a class="navbar-brand" href="#home" id="mainHead" contenteditable="false">Puppy Lounge</a>
-				<!--We can add logo here in between a tag-->
+  <div>
+    <nav
+      class="navbar navbar-expand-lg navbar-light bg-white navbar-transparent-light navbar-sticky"
+    >
+      <div class="container">
+        <a class="navbar-brand" href="#home" id="mainHead" contenteditable="false">Puppy Lounge</a>
+        <!--We can add logo here in between a tag-->
 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#landingkit-navbar"
-												aria-controls="landingkit-navbar" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#landingkit-navbar"
+          aria-controls="landingkit-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-				<div class="collapse navbar-collapse" id="landingkit-navbar">
-					<!--target="_blank" add when done-->
-					<ul class="navbar-nav ml-auto">
-						<li class="nav-item">
-							<a class="nav-link active" data-scroll href="#home">Home</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link active" data-scroll href="#about">About</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" data-scroll href="#roomRules">Room Rules</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" data-scroll href="https://jqbx.fm/" target="_blank">jqbx.fm</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" data-scroll href="https://www.last.fm/user/puppylounge" target="_blank">last.fm</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-		<hero-section></hero-section>
-		<about-section></about-section>
-		<events-section></events-section>
-		<rules-section></rules-section>
+        <div class="collapse navbar-collapse" id="landingkit-navbar">
+          <!--target="_blank" add when done-->
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link active" data-scroll href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" data-scroll href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-scroll href="#roomRules">Room Rules</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-scroll href="https://jqbx.fm/" target="_blank">jqbx.fm</a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link"
+                data-scroll
+                href="https://www.last.fm/user/puppylounge"
+                target="_blank"
+              >last.fm</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <hero-section></hero-section>
+    <about-section></about-section>
+    <events-section></events-section>
+    <rules-section></rules-section>
 
-		<footer class="footer">
-			<div class="text-center copyright">
-				<span>&copy; Copyright 2018-2019. Designed and Built by <a href="#">Fellow Puppers!</a></span>
-			</div>
-		</footer>
-
-	</div>
+    <footer class="footer">
+      <div class="text-center copyright">
+        <span>
+          &copy; Copyright 2018-2019. Designed and Built by
+          <a href="#">Fellow Puppers!</a>
+        </span>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
-	import HeroSection from '@/sections/HeroSection.vue';
-	import AboutSection from '@/sections/AboutSection.vue';
-	import EventsSection from '@/sections/EventsSection.vue';
-	import RulesSection from '@/sections/RulesSection.vue';
+import HeroSection from '@/sections/HeroSection.vue';
+import AboutSection from '@/sections/AboutSection.vue';
+import EventsSection from '@/sections/EventsSection.vue';
+import RulesSection from '@/sections/RulesSection.vue';
 
-	export default {
-		name: 'app',
-		components: {
-			HeroSection,
-			AboutSection,
-			EventsSection,
-			RulesSection
-		},
-	};
+export default {
+  name: 'app',
+  components: {
+    HeroSection,
+    AboutSection,
+    EventsSection,
+    RulesSection,
+  },
+};
 </script>
 
 <style lang="scss">
+*,
+p {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 
-	*, p {
-		box-sizing: border-box;
-		margin: 0;
-		padding: 0;
-	}
+body {
+  background: linear-gradient(to bottom, #ffffff, #5180af);
+}
 
-	body {
-		background: linear-gradient(to bottom, #ffffff, #5180af);
-	}
-
-	.editing {
-		-webkit-transition: all 0.30s ease-in-out;
-		-moz-transition: all 0.30s ease-in-out;
-		-ms-transition: all 0.30s ease-in-out;
-		-o-transition: all 0.30s ease-in-out;
-		outline: none;
-		padding: 16px 0px 16px 16px;
-		margin: 20px 4px 12px 0px;
-		border: 2px solid #44b5f7;
-		color: #FFF;
-		background: #010;
-	}
-
+.editing {
+  -webkit-transition: all 0.3s ease-in-out;
+  -moz-transition: all 0.3s ease-in-out;
+  -ms-transition: all 0.3s ease-in-out;
+  -o-transition: all 0.3s ease-in-out;
+  outline: none;
+  padding: 16px 0px 16px 16px;
+  margin: 20px 4px 12px 0px;
+  border: 2px solid #44b5f7;
+  color: #fff;
+  background: #010;
+}
 </style>
--- src/sections/HeroSection.vue
+++ src/sections/HeroSection.vue
@@ -5,12 +5,19 @@
       <div class="row align-items-center">
         <div class="col-lg-6 mr-auto ml-auto pb50">
           <h3 class="mb20 text-white">Welcome to the Puppy Lounge Site</h3>
-          <p class=" mb30 lead text-white-gray">
-            The puppy lounge is an open format room where you could listen to whatever you feel like listening to.
-          </p>
+          <p
+            class="mb30 lead text-white-gray"
+          >The puppy lounge is an open format room where you could listen to whatever you feel like listening to.</p>
           <div>
-            <a data-scroll href="https://app.jqbx.fm/room/puppy" target="_blank" class="btn btn-primary mr-3 my-3">Join
-              Us</a>
+            <a
+              data-scroll
+              href="https://app.jqbx.fm/room/puppy"
+              target="_blank"
+              class="btn btn-primary mr-3 my-3"
+            >
+              Join
+              Us
+            </a>
           </div>
         </div>
 
@@ -18,13 +25,89 @@
           <div class="carousel-outer">
             <div class="owl-carousel owl-theme carousel-browser">
               <div class="item">
-                <img src="https://i.imgur.com/gNuIcJL.png" alt="Artist: Frank Ocean">
+                <img
+                  class="feature-image"
+                  src="https://i.imgur.com/U2GSGrh.jpg"
+                  alt="Artist: Mac Miller"
+                />
               </div>
               <div class="item">
-                <img src="https://i.imgur.com/10olGfj.jpg" alt="Artist: Saba">
+                <img
+                  class="feature-image"
+                  src="https://i.imgur.com/YMsqt4T.jpg"
+                  alt="Artist: Drake"
+                />
               </div>
               <div class="item">
-                <img src="https://i.imgur.com/4NiUyR2.jpg" alt="Artist: Mac Miller">
+                <img
+                  class="feature-image"
+                  src="https://i.imgur.com/lDt1MdK.jpg"
+                  alt="Artist: Kendrick Lamar"
+                />
+              </div>
+              <div class="item">
+                <img
+                  class="feature-image"
+                  src="https://i.imgur.com/Cc7Llnp.jpg"
+                  alt="Artist: Kanye West"
+                />
+              </div>
+              <div class="item">
+                <img
+                  class="feature-image"
+                  src="https://i.imgur.com/ftApk3x.jpg"
+                  alt="Artist: Frank Ocean"
+                />
+              </div>
+              <div class="item">
+                <img
+                  class="feature-image"
+                  src="https://i.imgur.com/z1HpQIE.jpg"
+                  alt="Artist: BROCKHAMPTON"
+                />
+              </div>
+              <div class="item">
+                <img
+                  class="feature-image"
+                  src="https://i.imgur.com/NXWgFXc.jpg"
+                  alt="Artist: J. Cole"
+                />
+              </div>
+              <div class="item">
+                <img class="feature-image" src="https://i.imgur.com/10olGfj.jpg" alt="Artist: Saba" />
+              </div>
+              <div class="item">
+                <img
+                  class="feature-image"
+                  src="https://i.imgur.com/D9qzxRP.jpg"
+                  alt="Artist: Anderson .Paak"
+                />
+              </div>
+              <div class="item">
+                <img
+                  class="feature-image"
+                  src="https://i.imgur.com/0jRlViy.jpg"
+                  alt="Artist: The Weeknd"
+                />
+              </div>
+              <div class="item">
+                <img
+                  class="feature-image"
+                  src="https://i.imgur.com/1szY3gX.jpg"
+                  alt="Artist: The Internet"
+                />
+              </div>
+              <div class="item">
+                <img src="https://i.imgur.com/8WHqslq.jpg" alt="Artist: Childish Gambino" />
+              </div>
+              <div class="item">
+                <img src="https://i.imgur.com/Mc14P6L.jpg" alt="Artist: Atmosphere" />
+              </div>
+              <div class="item">
+                <img src="https://i.imgur.com/SLVd0tv.jpg" alt="Artist: Toro Y Moi" />
+              </div>
+              <div class="item">
+                <img src="https://i.imgur.com/uXw74Tr.jpg" alt="Artist: People Under The Stairs" />
               </div>
             </div>
           </div>
@@ -37,80 +120,75 @@
 
 <script>
 export default {
-  name: 'hero-section',
-  props: {
-
-  },
+  name: "hero-section",
+  props: {},
   data() {
-    return {
-
-    };
+    return {};
   },
-  methods: {
-
-  },
+  methods: {}
 };
 </script>
 
 <style lang="scss">
-	@media(max-width: 991px) {
-		.hero-gradient-dark {
-			padding-bottom: 0rem;
-			padding-top: 4rem;
-		}
-		.navbar-toggler {
-			position: absolute;
-			right: 15px;
-			background-image: none;
-			border: 0px;
-			padding: 0px;
-			top: 0;
-			.navbar-toggler-icon {
-				background-image: none;
-				&:after {
-					font-family: 'Linearicons';
-					content: "\e92b";
-					position: absolute;
-					right: 0;
-					font-size: 2em;
-					top: 3px;
-					transition: all .3s;
-					-webkit-transition: all .3s;
-					color: #000;
-					cursor: pointer;
-				}
-			}
-		}
-		.navbar .navbar-nav .nav-link {
-			border-top: 0px;
-		}
-		.navbar.navbar-expand-lg {
-			.nav-link.btn {
-				display: none;
-			}
-			.navbar-collapse.show {
-				height: 250px;
-				overflow-y: scroll;
-			}
-		}
-		.dropdown-menu {
-			box-shadow: none;
-		}
-		.feature-img-section {
-			text-align: center;
-		}
-		.feature-list li {
-			padding-left: 21px;
-			margin-left: 20px;
-			display: inline-block;
-		}
-		.feature-img {
-			position: static;
-			margin-bottom: 30px;
-			width: 100%;
-			display: table;
-			margin: 0 auto;
-			display: block;
-		}
-	}
+@media (max-width: 991px) {
+  .hero-gradient-dark {
+    padding-bottom: 0rem;
+    padding-top: 4rem;
+  }
+  .navbar-toggler {
+    position: absolute;
+    right: 15px;
+    background-image: none;
+    border: 0px;
+    padding: 0px;
+    top: 0;
+    .navbar-toggler-icon {
+      background-image: none;
+      &:after {
+        font-family: "Linearicons";
+        content: "\e92b";
+        position: absolute;
+        right: 0;
+        font-size: 2em;
+        top: 3px;
+        transition: all 0.3s;
+        -webkit-transition: all 0.3s;
+        color: #000;
+        cursor: pointer;
+      }
+    }
+  }
+  .navbar .navbar-nav .nav-link {
+    border-top: 0px;
+  }
+  .navbar.navbar-expand-lg {
+    .nav-link.btn {
+      display: none;
+    }
+    .navbar-collapse.show {
+      height: 250px;
+      overflow-y: scroll;
+    }
+  }
+  .dropdown-menu {
+    box-shadow: none;
+  }
+  .feature-img-section {
+    text-align: center;
+  }
+  .feature-list li {
+    padding-left: 21px;
+    margin-left: 20px;
+    display: inline-block;
+  }
+  .feature-img {
+    position: static;
+    margin-bottom: 30px;
+    width: 100%;
+    height: 100%;
+    display: table;
+    margin: 0 auto;
+    display: block;
+  }
+}
 </style>
