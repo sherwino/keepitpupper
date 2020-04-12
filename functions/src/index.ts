1627538c21294f3b13f70394a667f77144100bcd
sherwino <s.garcia@applicaster.com>
2020-04-12 01:17:36 -0400

    feat: initial firebase fn setup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,26 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+admin.initializeApp();
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+export const addMessage = functions.https.onRequest(async (req, res) => {
+	// Grab the text parameters
+	const original = req?.query?.text;
+
+	// Push a message into the database using the Firebase Admin SDK
+	// Need to confirm that this works with the firestore
+	// Honestly don't care which one we use 😅
+	const snapshot = await admin
+		.database()
+		.ref("/messages")
+		.push({ original });
+
+	// Redirect with 303 SEE othere to the url of the pushed object in the firebase console
+	// Honestly don't know what this does need to look it up
+	res.redirect(303, snapshot?.ref.toString());
+});
